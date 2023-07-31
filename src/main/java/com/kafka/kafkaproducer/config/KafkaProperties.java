78d23e9a983c5ea229211184dbba286b7084bfcc
chlee <changhoon1994@gmail.com>
2023-07-31 16:53:56 +0900

    properties

--- src/main/java/com/kafka/kafkaproducer/config/KafkaProperties.java
+++ src/main/java/com/kafka/kafkaproducer/config/KafkaProperties.java
@@ -5,12 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 
-@ConfigurationProperties("kafka")
+@ConfigurationProperties("spring.kafka")
 @Getter
 @Setter
 public class KafkaProperties {
-    @Value("localhost:9092")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServers;
 }
